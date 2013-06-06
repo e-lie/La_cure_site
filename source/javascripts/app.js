219a77f21eda2059eb88bb1c80e94163bfbfdd0c
eliie <eliie@eliie-1005HAG.(none)>
2013-06-06 23:39:56 +0200

    parallax background effect

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -1,3 +1,5 @@
 $(document).ready(function () {
-
+    //$('#parallax-bg').css('background-color', 'red');
+    //$('#parallax-bg').css('background-image', 'none');
+    $('#parallax-bg').parallax("50%", 0);
 });
